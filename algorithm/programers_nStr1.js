29d8abfe70ddf0c8cc5fd118e62eeab326195c91
orrhrr <orrhrr@gmail.com>
2023-07-21 12:00:19 +0900

    object type study

---
+++ algorithm/programers_nStr1.js
@@ -0,0 +1,15 @@
+// 문자열 뒤의 n글자
+
+function solution(my_string, n) {
+    var answer = '';
+    for(let i = 0; i < my_string.length; i++) {
+        if(i >= my_string.length - n ){
+            answer += my_string[i];
+        }
+      //  자른문자열 길이 = 총문자길이 - n
+      // answer.length = my_string.length-n;
+        
+    }
+
+    return answer;
+}
