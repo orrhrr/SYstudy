92332fda9e6449d02ab7df5ff7e398a7b5520873
orrhrr <orrhrr@gmail.com>
2023-08-16 16:39:49 +0900

    algorithm level 0

---
+++ algorithm/programers_0/differentOperationsOfLengthOfArray.js
@@ -0,0 +1,33 @@
+/*
+배열의 길이에 따라 다른 연산하기
+문제 설명
+정수 배열 arr과 정수 n이 매개변수로 주어집니다. arr의 길이가 홀수라면 arr의 모든 짝수 인덱스 위치에 n을 더한 배열을, arr의 길이가 짝수라면 arr의 모든 홀수 인덱스 위치에 n을 더한 배열을 return 하는 solution 함수를 작성해 주세요.
+
+제한사항
+1 ≤ arr의 길이 ≤ 1,000
+1 ≤ arr의 원소 ≤ 1,000
+1 ≤ n ≤ 1,000
+입출력 예
+arr	n	result
+[49, 12, 100, 276, 33]	27	[76, 12, 127, 276, 60]
+[444, 555, 666, 777]	100	[444, 655, 666, 877]
+*/
+
+function solution(arr, n) {
+    var answer = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(arr.length % 2 === 1) {
+            i % 2 === 0 ? answer[i] = arr[i] + n : answer[i] = arr[i];
+        } else {
+            i % 2 === 1 ? answer[i] = arr[i] + n : answer[i] = arr[i];
+        }
+    }
+    return answer;
+}
+
+// map 메서드 사용
+const solution = (arr, n) => arr.map((num, idx) => (
+    arr.length % 2 !== idx % 2
+    ? num + n
+    : num
+))
