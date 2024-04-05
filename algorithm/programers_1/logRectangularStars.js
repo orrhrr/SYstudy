e03b514dddadbf8930fdc59fd3d95d4e52c430b2
orrhrr <orrhrr@gmail.com>
2024-04-05 18:39:49 +0900

    algorithm level 1: 직사각형 별찍기

---
+++ algorithm/programers_1/logRectangularStars.js
@@ -0,0 +1,43 @@
+// 직사각형 별찍기
+
+/*
+이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+*/
+
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  let star = "";
+  for (let i = 0; i < b; i++) {
+    for (let j = 0; j < a; j++) {
+      star += "*";
+    }
+    star += "\n";
+  }
+  console.log(star);
+});
+
+// 다른 사람의 풀이 1
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  const row = "*".repeat(a);
+  for (let i = 0; i < b; i++) {
+    console.log(row);
+  }
+});
+// 다른 사람의 풀이 2
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  const star = `${"*".repeat(a)}\n`;
+
+  console.log(star.repeat(b));
+});
