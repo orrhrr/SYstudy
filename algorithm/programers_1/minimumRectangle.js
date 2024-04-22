04bac248d810f16055557a54133c4df6a6846f23
orrhrr <orrhrr@gmail.com>
2024-04-22 15:45:20 +0900

    algorithm level 1: 최소직사각형

---
+++ algorithm/programers_1/minimumRectangle.js
@@ -0,0 +1,59 @@
+// 최소직사각형
+/* 명함 지갑을 만드는 회사에서 지갑의 크기를 정하려고 합니다. 다양한 모양과 크기의 명함들을 모두 수납할 수 있으면서, 작아서 들고 다니기 편한 지갑을 만들어야 합니다. 이러한 요건을 만족하는 지갑을 만들기 위해 디자인팀은 모든 명함의 가로 길이와 세로 길이를 조사했습니다.
+
+아래 표는 4가지 명함의 가로 길이와 세로 길이를 나타냅니다.
+
+명함 번호	가로 길이	세로 길이
+1	60	50
+2	30	70
+3	60	30
+4	80	40
+가장 긴 가로 길이와 세로 길이가 각각 80, 70이기 때문에 80(가로) x 70(세로) 크기의 지갑을 만들면 모든 명함들을 수납할 수 있습니다. 하지만 2번 명함을 가로로 눕혀 수납한다면 80(가로) x 50(세로) 크기의 지갑으로 모든 명함들을 수납할 수 있습니다. 이때의 지갑 크기는 4000(=80 x 50)입니다.
+
+모든 명함의 가로 길이와 세로 길이를 나타내는 2차원 배열 sizes가 매개변수로 주어집니다. 모든 명함을 수납할 수 있는 가장 작은 지갑을 만들 때, 지갑의 크기를 return 하도록 solution 함수를 완성해주세요.
+*/
+
+function solution(sizes) {
+  var answer = 0;
+  let length = sizes.map((size) => size[0]);
+  let width = sizes.map((size) => size[1]);
+  let total = [];
+
+  const lengthMax = Math.max(...length);
+  const widthMax = Math.max(...width);
+  const totalMax = Math.max(lengthMax, widthMax);
+
+  // 최대 세로(또는 가로)길이가 최대 가로(또는 세로)길이보다 크면
+  // 최대 값은 그중 큰 세로(또는 가로)길이를 포함할 수 있다.
+  // 가로/세로 중 작은 값이 가장 큰 수
+
+  for (let i = 0; i < length.length; i++) {
+    total.push(Math.min(length[i], width[i]));
+  }
+
+  return (answer = Math.max(...total) * totalMax);
+}
+
+// 다른 사람의 풀이
+function solution2(sizes) {
+  const rotated = sizes.map(([w, h]) => (w < h ? [h, w] : [w, h]));
+
+  let maxSize = [0, 0];
+  rotated.forEach(([w, h]) => {
+    if (w > maxSize[0]) maxSize[0] = w;
+    if (h > maxSize[1]) maxSize[1] = h;
+  });
+  return maxSize[0] * maxSize[1];
+}
+
+function solution3(sizes) {
+  let w = 0;
+  let h = 0;
+  sizes.forEach((s) => {
+    const [a, b] = s.sort((a, b) => a - b);
+    if (a > h) h = a;
+    if (b > w) w = b;
+  });
+
+  return w * h;
+}
